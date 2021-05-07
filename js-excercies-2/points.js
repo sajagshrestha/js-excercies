02ccda65c5bbb6a9e785d2a89aa60d6ebb1c72ba
Sajag Shrestha <sajagshrestha0852@gmail.com>
2021-05-07 11:54:28 +0545

    points added

---
+++ js-excercies-2/points.js
@@ -0,0 +1,24 @@
+var container = document.querySelector("#pointsContainer");
+var points = [
+	{ x: 20, y: 30 },
+	{ x: 200, y: 40 },
+	{ x: 120, y: 300 },
+	{ x: 205, y: 60 },
+	{ x: 421, y: 10 },
+];
+
+points.forEach((point) => {
+	var newPoint = document.createElement("div");
+	newPoint.classList.add("point");
+	newPoint.style.top = `${point.y}px`;
+	newPoint.style.left = `${point.x}px`;
+	container.appendChild(newPoint);
+});
+
+var plottedPoints = document.querySelectorAll(".point");
+
+plottedPoints.forEach((plottedPoint) => {
+	plottedPoint.addEventListener("click", (e) => {
+		container.removeChild(e.target);
+	});
+});
